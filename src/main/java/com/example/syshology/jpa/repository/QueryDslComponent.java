79f8425ec91aefd0cfa80f64219204cf9d113563
김상희 <omen444@gmail.com>
2021-02-22 16:46:32 +0900

    JPA 성능 및 이해 AUTO QUERY 테스트 코드 작성

---
+++ src/main/java/com/example/syshology/jpa/repository/QueryDslComponent.java
@@ -0,0 +1,11 @@
+package com.example.syshology.jpa.repository;
+
+/**
+ * Created by s.h.kim.
+ * User: 김상희
+ * Date: 2021-02-22
+ * Time: 오후 4:46
+ * Project : IntelliJ IDEA
+ */
+public class QueryDslComponent {
+}
